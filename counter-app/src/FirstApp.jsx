a2ce229bfbb4c5eea6abeb581b349f0f669d2a7f
Ulifly <ulifly@hotmail.com>
2025-03-30 22:20:34 -0600

    update

--- counter-app/src/FirstApp.jsx
+++ counter-app/src/FirstApp.jsx
@@ -5,11 +5,12 @@ import React from 'react';
 export const FirstApp = ( {
     //default props
     title = 'No hay titulo',
-    subtitle = 'No hay subtitulo'
+    subtitle = 'No hay subtitulo',
+    nombre = 'nameless'
 } ) => {
     return (
         <>
-            <h1>Hola</h1>  
+            <h1>Hola {nombre}</h1>  
             <h2>{title}</h2> 
             <h3>{subtitle}</h3>
         </>
@@ -19,12 +20,7 @@ export const FirstApp = ( {
 // antes asi se declaraban los defaultProps
 
 
-// FirstApp.propTypes = {
-//     title: PropTypes.string.isRequired,
-//     subtitle: PropTypes.string.isRequired
-// }
-
-// FirstApp.defaultProps = {
-//     title: 'No hay titulo',
-//     subtitle: 'No hay subtitulo'
-// };
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+}
